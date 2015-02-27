cba923eac01370512461dd124135334ad5f9a4f7
Jose A. Rilla <jose@rilla.es>
2015-02-27 12:54:13 +0100

    Handle projects without issues

--- lib/dradis/plugins/csv/exporter.rb
+++ lib/dradis/plugins/csv/exporter.rb
@@ -5,25 +5,31 @@ module Dradis::Plugins::CSV
       template        = args[:template]
 
       issues = content_service.all_issues
-      keys = issues.first.fields.keys
 
-      # Create the CSV data
-      csv_string = ::CSV.generate do |csv|
+      if issues.empty?
+        return "The project didn't contain any issues"
+      else
+        keys = issues.first.fields.keys
 
-        # Add the header line with all the field names. Remember note.field returns
-        # a hash: {'Title' => 'Foo', 'Description' => 'Bar'}
-        csv << keys
+        # Create the CSV data
+        csv_string = ::CSV.generate do |csv|
 
-        # For each of the notes in our category, we dump the field values.
-        # This assumes all notes have the same fields, in the same order
-        issues.each do |issue|
-          csv << keys.map do |key|
-            issue.fields.fetch(key, 'n/a')
+          # Add the header line with all the field names. Remember note.field returns
+          # a hash: {'Title' => 'Foo', 'Description' => 'Bar'}
+          csv << keys
+
+          # For each of the notes in our category, we dump the field values.
+          # This assumes all notes have the same fields, in the same order
+          issues.each do |issue|
+            csv << keys.map do |key|
+              issue.fields.fetch(key, 'n/a')
+            end
           end
         end
+
+        csv_string
       end
 
-      csv_string
     end
   end
 end
