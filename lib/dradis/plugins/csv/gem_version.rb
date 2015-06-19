df7f509a22f74891ca712cfb407ba7be6183bc88
George Millo <georgejulianmillo@gmail.com>
2015-06-19 12:34:00 +0200

    bumping version to 3.3

--- lib/dradis/plugins/csv/gem_version.rb
+++ lib/dradis/plugins/csv/gem_version.rb
@@ -8,9 +8,9 @@ module Dradis
 
       module VERSION
         MAJOR = 3
-        MINOR = 0
-        TINY = 1
-        PRE = nil
+        MINOR = 3
+        TINY  = 0
+        PRE   = nil
 
         STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
       end
