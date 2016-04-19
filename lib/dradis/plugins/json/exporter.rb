80554bd8e8c68168a1a98e1c183f58dfe92694b3
Daniel Martin <etd-github@nomejortu.com>
2016-04-19 09:26:19 +0200

    Exporter - simplify processing with #map

--- lib/dradis/plugins/json/exporter.rb
+++ lib/dradis/plugins/json/exporter.rb
@@ -5,20 +5,11 @@ module Dradis::Plugins::JSON
 
       issues = content_service.all_issues
 
-      if issues.empty?
-        return "The project didn't contain any issues"
-      else
-        require 'rubygems'
-        require 'json/pure'
-
-        # Add a blank array to fill with the Issue data 
-        array = []
-        # Each issue in the project is added as a new hash to the array
-        issues.each do |issue|
-          array << {:issue => issue.fields}
-        end
-        return JSON.pretty_generate(array, :indent => "\t")
+      result = issues.map do |issue|
+        { issue: issue.fields }
       end
+
+      JSON.pretty_generate(result)
     end
   end
 end
