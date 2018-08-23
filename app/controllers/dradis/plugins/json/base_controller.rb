2f186b0d586ead45abd70865485743ed789140fe
Xavi Vila <xavivm@gmail.com>
2018-08-23 11:07:50 +0200

    update base_controller to work with latest version of dradis_plugins

--- app/controllers/dradis/plugins/json/base_controller.rb
+++ app/controllers/dradis/plugins/json/base_controller.rb
@@ -4,17 +4,10 @@ module Dradis
       class BaseController < Dradis::Plugins::Export::BaseController
 
         def index
-          # these come from Export#create
-          export_manager_hash   = session[:export_manager].with_indifferent_access
-          content_service_class = export_manager_hash[:content_service].constantize
+          exporter = Dradis::Plugins::Json::Exporter.new(export_options)
+          json     = exporter.export
 
-          exporter = Dradis::Plugins::Json::Exporter.new(
-            content_service: content_service_class.new(plugin: Dradis::Plugins::Json)
-          )
-
-          doc = exporter.export(export_manager_hash)
-
-          render text: doc
+          render json: json
         end
       end
 
